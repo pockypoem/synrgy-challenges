360ec4ac81db786f33f7d6506949a4e54da32788
pockypoem <jeremyas.cornelis@gmail.com>
2023-10-10 21:20:10 +0700

    improvement

--- chapter03/latihanSoal/02.js
+++ chapter03/latihanSoal/02.js
@@ -1,13 +1,21 @@
 const nilaiMahasiswa = parseFloat(prompt("Masukkan Nilai Mahasiswa: "));
+const kategori = "E";
 
-if (nilaiMahasiswa >= 91 && nilaiMahasiswa <= 100) {
-    console.log("Kategori: A");
-} else if (nilaiMahasiswa >= 81 && nilaiMahasiswa <= 90) {
-    console.log("Kategori: B");
-} else if (nilaiMahasiswa >= 71 && nilaiMahasiswa <= 80) {
-    console.log("Kategori: C");
-} else if (nilaiMahasiswa >= 61 && nilaiMahasiswa <= 70) {
-    console.log("Kategori: D");
-} else {
-    console.log("Kategori: E");
+if(!(nilaiMahasiswa >= 0 && nilaiMahasiswa <= 100)) {
+    return "Nilai harus diantara 0 hingga 100"
 }
+
+if (nilaiMahasiswa >= 0 ) {
+    if (nilaiMahasiswa >= 91 ) {
+        kategori = "A";
+    } else if (nilaiMahasiswa >= 81) {
+        kategori = "B";
+    } else if (nilaiMahasiswa >= 71) {
+        kategori = "C";
+    } else if (nilaiMahasiswa >= 61) {
+        kategori = "D";
+    }
+}
+
+
+console.log("Kategori: " + kategori);
