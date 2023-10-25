e615b6cba152d268c49412ccbf2f4565e50f5298
pockypoem <jeremyas.cornelis@gmail.com>
2023-10-25 08:45:43 +0700

    success fetching data and filter data

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -33,13 +33,63 @@ class App {
         const selectedPickupTime = this.pickupTime.value;
         const selectedJumlahPenumpang = this.jumlahPenumpang.value;
 
-
-        console.log('Tipe Driver: ' + selectedTipeDriver);
-        console.log('Tanggal: ' + selectedTanggal);
-        console.log('Waktu Jemput: ' + selectedPickupTime);
-        console.log('Jumlah Penumpang: ' + selectedJumlahPenumpang);
+        this.displayFormData(selectedTipeDriver, selectedTanggal, selectedPickupTime, selectedJumlahPenumpang);
+        this.getCarsData(e, selectedTanggal, selectedPickupTime);
 
         this.resetForm();
+    }
+
+    convertTimeToWIB(isoFormatDateTime) {
+        const dateObj = new Date(isoFormatDateTime);
+        dateObj.setHours(dateObj.getHours() + 7);
+        return dateObj;
+    }
+
+    async getCarsData(e, inputTanggal, waktuJemput) {
+        e.preventDefault();
+
+        const url = 'https://raw.githubusercontent.com/pockypoem/synrgy-challenges/challenge04/data/cars.min.json';
+        const response = await fetch(url);
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+
+        // Mengonversi nilai properti "availableAt" menjadi array ["YYYY-MM-DD", "HH:MM"]
+        const convertedData = jsonResponse.map(item => {
+            const dateObj = this.convertTimeToWIB(item.availableAt);
+            const wibDate = dateObj.toISOString().split('T')[0];
+            const wibTime = dateObj.toISOString().split('T')[1].slice(0,5);
+            return {
+                ...item,
+                availableAt: [wibDate, wibTime],
+            };
+        });
+
+        const availableCars = convertedData.filter(car => {
+            const [dataDate, dataTime] = car.availableAt;
+            const [dataHours, dataMinutes] = dataTime.split(':');
+
+            const [inputHours, inputMinutes] = waktuJemput.split(':');
+            
+            if(dataDate === inputTanggal) {
+                if(inputHours > dataHours) {
+                    return true;
+                } else if(inputHours === dataHours && inputMinutes >= dataMinutes) {
+                    return true;
+                }
+            }
+            return false;
+        });
+        
+        console.log(availableCars);
+        
+
+    }
+
+    displayFormData(tipeDriver, tanggal, pickupTime, jumlahPenumpang) {
+        console.log('Tipe Driver: ' + tipeDriver);
+        console.log('Tanggal: ' + tanggal);
+        console.log('Waktu Jemput: ' + pickupTime);
+        console.log('Jumlah Penumpang: ' + jumlahPenumpang);
     }
 
 }
